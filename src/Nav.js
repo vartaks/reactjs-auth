053a95e9956d1a8d553415ca687566dcaee7c875
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-25 00:51:38 +0530

    Added a Private API using NodeJS and Express library

--- src/Nav.js
+++ src/Nav.js
@@ -17,6 +17,11 @@ class Nav extends Component {
                     <li>
                         <Link to='/public'>Public</Link>
                     </li>
+                    {isAuthenticated() && (
+                        <li>
+                            <Link to='/private'>Private</Link>
+                        </li>
+                    )}
                     <li>
                         <button onClick={isAuthenticated() ? logout : login}>
                             {isAuthenticated() ? "Log Out" : "Log In"}
